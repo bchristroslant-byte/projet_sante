9a5244d636328312cdee84fb7a2064c5057f05c6
bchristroslant-byte <bchristroslant@gmail.com>
2026-06-26 10:32:59 +0200

    Read_md

---
+++ READ.ME.py
@@ -0,0 +1,28 @@
+
+# Taux de change
+fcfa_euro = 656.957
+fcfa_usd = 600.0
+# Seuils OMS
+seuil_densite = 0.05
+seuil_mortalite = 2
+# Département du Congo
+departement = ["Brazzaville",
+               "Pointe-Noire",
+               "Kouilou",
+               "Lekoumou",
+               "Niari",
+               "Bouenza",
+               "Pool",
+               "Plateaux",
+               "Cuvette",
+               "Cuvette ouest"
+               "Sangha"
+               "Likouala"
+]
+# Variables hôpitaux
+hopital = ["CHU Brazzaville",
+           "Hôpital General Pointe-Noire",
+           "Hôpital de dolisie",
+           "Hopital de district Owando",
+           "Centre de sante d'Impfonde"
+]
